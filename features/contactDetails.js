72f04c5ccfe0f45be453ad5ce3b8cba1806b29a2
Sandali Lokuge <sdewnilokuge@gmail.com>
2017-11-27 23:54:47 +0530

    testing 1

--- features/contactDetails.js
+++ features/contactDetails.js
@@ -1,7 +1,7 @@
 module.exports = bp => {
     bp.hear({'wit.entities.intent[0].value': 'get_contact_details'}, (event, next) => {
     console.log('>> get_contact_details')
-    event.reply('#goodbye', {
+    event.reply('#contactReply', {
       // You can pass data to the UMM bloc!
       info: 'wit.entities.intent[0].value'
     })
