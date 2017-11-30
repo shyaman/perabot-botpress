51984c30fb99ce93af78d9ad3e87f15699380d5c
shyaman_madhawa <shayaman321@gmail.com>
2017-11-30 20:17:50 +0530

    halfy done

--- features/contactDetails.js
+++ features/contactDetails.js
@@ -4,7 +4,7 @@ module.exports = bp => {
   // when 'get_contact_information' intent trigger
     bp.hear({'nlp.metadata.intentName': 'get_contact_information'}, (event, next) => {
 
-      let information_type = _.get(event, 'nlp.parameters.information_type')
+      const information_type = _.get(event, 'nlp.parameters.information_type')
       let person = _.get(event, 'nlp.parameters.person')
 
       bp.db.get().then(knex => knex('contacts').where({name : person}))
@@ -12,12 +12,25 @@ module.exports = bp => {
         const email = _.get(contact[0],'email')
         const phoneNumber = _.get(contact[0],'telephone')
 
-        event.reply('#contactReply',{
-          person:person,
-          information_type:information_type,
-          email:email,
-          phoneNumber:phoneNumber
-        });
+        if (information_type == '') {
+          event.reply('#contactReply',{
+            person:person,
+            email:email,
+            phoneNumber:phoneNumber
+          });
+        } else if (information_type == 'Email address') {
+          event.reply('#contactReplyDistinct',{
+            person:person,
+            information: email,
+            information_type:information_type
+          });
+        } else if (information_type == 'Phone number') {
+          event.reply('#contactReplyDistinct',{
+            person:person,
+            information: phoneNumber,
+            information_type:information_type
+          });
+        }
       })
 
   })
