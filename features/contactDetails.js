24a6a048a2e2a94a694df755adda3bf4820912b4
Puwasuru Ihalagedara <puwasuru95@gmail.com>
2017-11-30 23:20:00 +0530

    add context to contact Details

--- features/contactDetails.js
+++ features/contactDetails.js
@@ -33,5 +33,32 @@ module.exports = bp => {
         }
       })
 
-  })
+    })
+
+    bp.hear({'nlp.metadata.intentName': 'get_contact_information_with_context'}, (event, next) => {
+
+        const information_type = _.get(event, 'nlp.parameters.information_type')
+        let person = _.get(event, 'nlp.parameters.person')
+
+        bp.db.get().then(knex => knex('contacts').where({name : person}))
+        .then(contact =>{
+          const email = _.get(contact[0],'email')
+          const phoneNumber = _.get(contact[0],'telephone')
+
+          if (information_type == 'Email address') {
+            event.reply('#contactReplyDistinct',{
+              person:person,
+              information: email,
+              information_type:information_type
+            });
+          } else if (information_type == 'Phone number') {
+            event.reply('#contactReplyDistinct',{
+              person:person,
+              information: phoneNumber,
+              information_type:information_type
+            });
+          }
+        })
+
+    })
 }
