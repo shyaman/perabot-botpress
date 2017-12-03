f7d2aab43abad7a40ad21c2e9071427c15e6c6bb
shyaman_madhawa <shayaman321@gmail.com>
2017-12-03 17:00:53 +0530

    daily timetable

--- features/timeTable.js
+++ features/timeTable.js
@@ -11,7 +11,7 @@ module.exports = bp =>{
       auth: jwtClient,
       calendarId: 'mhu2j2t8ar0vea03t7orsmrbts@group.calendar.google.com',
       timeMin: (new Date()).toISOString(),
-      timeMax:(new Date(new Date().getTime()+1000*60*60*24)).toISOString(),
+      timeMax:(new Date(new Date().setHours(18,1,0))).toISOString(),
       maxResults: 2,
       singleEvents: true,
       orderBy: 'startTime'
@@ -39,6 +39,40 @@ module.exports = bp =>{
                }
            }
            event.reply('#noSubject')
+       }
+    });
+  })
+
+  bp.hear({'nlp.metadata.intentName': 'get_timetable_daily'}, (event, next) =>{
+
+    const getDate = _.get(event, 'nlp.parameters.date')
+    const splitedDate = getDate.split("-")
+    const date = new Date(new Date().setFullYear(splitedDate[0],splitedDate[1]-1,splitedDate[2]))
+
+    calendar.events.list({
+      auth: jwtClient,
+      calendarId: 'mhu2j2t8ar0vea03t7orsmrbts@group.calendar.google.com',
+      timeMin: (new Date(date.setHours(8,0,0))).toISOString(),
+      timeMax:(new Date(date.setHours(18,1,0))).toISOString(),
+      singleEvents: true,
+      orderBy: 'startTime'
+    },  (err, response) => {
+       if (err) {
+           console.log('The API returned an error: ' + err);
+           return;
+       }
+       var calEvents = response.items;
+       if (calEvents.length == 0) {
+           event.reply('#noSubject')
+       } else {
+           const curTime = new Date();
+           for (let calEvent of response.items) {
+               event.reply('#dailyTimetable',{
+                 subject:_.get(calEvent,'summary'),
+                 startTime:new Date(calEvent.start.dateTime).toLocaleTimeString(),
+                 endTime:new Date(calEvent.end.dateTime).toLocaleTimeString()
+               })
+           }
        }
     });
   })
