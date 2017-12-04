633ae9419215295dc3d2d39d7ad923d27bdb3203
Sandali Lokuge <sdewnilokuge@gmail.com>
2017-12-04 11:55:17 +0530

    Add Yes/No

--- features/contactDetails.js
+++ features/contactDetails.js
@@ -1,13 +1,15 @@
 const _ = require('lodash');
 
 module.exports = bp => {
-
+  const utterances = {
+    yes: /Yes|yeah|yes|ok/i,
+    no: /nope|no/i,
+  }
   // when 'get_contact_information' intent trigger
     bp.hear({'nlp.metadata.intentName': 'get_contact_information'}, (event, next) => {
 
       const information_type = _.get(event, 'nlp.parameters.information_type')
       let person = _.get(event, 'nlp.parameters.person')
-
 
         bp.convo.start(event, convo => {
           if (person == '') {   //convo ask for the person
@@ -39,7 +41,7 @@ module.exports = bp => {
               const phoneNumber = _.get(contact[0],'telephone')
 
               if(contact.length != 0){
-                if (information_type == '') {
+                if (information_type == '' || information_type =='Email Address and Phone Number') {
                   if(email != '' && phoneNumber != ''){
                     event.reply('#contactReply',{
                       person:person,
@@ -87,16 +89,99 @@ module.exports = bp => {
 
                 }
               }else{
+
                 bp.db.get().then(knex => knex('contacts').whereRaw('LOWER(name) LIKE ?', '%'+splitName[0].toLowerCase()+'%').select('name'))
                 .then(suggestions =>{
                   var suggestedNames=[]
+                  var res=""
                   for (var i = 0; i < suggestions.length; i++) {
                       suggestedNames.push(_.get(suggestions[i],'name'));
                   }
-                  if(suggestions.length !=0){
+                  /*if(suggestions.length !=0){
                     event.reply('#suggestNames',{
                       suggestions:suggestedNames
-                    })
+                    })*/
+                  bp.convo.start(event, convo => {
+                  if(suggestions.length ==1){
+                    convo.threads['default'].addQuestion('#suggestNames',{suggestions:suggestedNames}, [
+                    {
+                      pattern: utterances.yes,
+                      callback: () => {
+                        res = "yes";
+                        person=_.get(suggestions[0],'name')
+                        console.log(res)
+                        console.log(person)
+                        convo.next();
+                      }
+                    },
+                    {
+                      default: true,    //if its not in correct pattern, question repeat
+                      callback: () => {
+                        convo.repeat()
+                      }
+                    }
+                  ]);
+                  convo.on('done', () => {
+                    if(res=="yes"){
+                      bp.db.get().then(knex => knex('contacts').whereRaw('LOWER(name) LIKE ?', person.toLowerCase()))
+                      .then(contact =>{
+                        const email = _.get(contact[0],'email')
+                        const phoneNumber = _.get(contact[0],'telephone')
+
+                        if (information_type == '' || information_type =='Email Address and Phone Number') {
+                          if(email != '' && phoneNumber != ''){
+                            event.reply('#contactReply',{
+                              person:person,
+                              email:email,
+                              phoneNumber:phoneNumber
+                            });
+                          }else if(email != '' && phoneNumber == ''){
+                            event.reply('#telErrorReply',{
+                              person:person,
+                              email:email
+                            });
+                          }else if(email == '' && phoneNumber != ''){
+                            event.reply('#emailErrorReply',{
+                              person:person,
+                              phoneNumber:phoneNumber
+                            });
+                          }
+                        } else if (information_type == 'Email address') {
+                          if(email != ''){
+                            event.reply('#contactReplyDistinct',{
+                              person:person,
+                              information: email,
+                              information_type:information_type
+                            });
+                          }else{
+                            event.reply('#contactErrorReply',{
+                              person:person,
+                              information_type:information_type
+                            });
+                          }
+
+                        } else if (information_type == 'Phone number') {
+                          if(phoneNumber != ''){
+                            event.reply('#contactReplyDistinct',{
+                              person:person,
+                              information: phoneNumber,
+                              information_type:information_type
+                            });
+                          }else{
+                            event.reply('#contactErrorReply',{
+                              person:person,
+                              information_type:information_type
+                            });
+                          }
+
+                        }
+
+                      })
+                    }else{
+                      event.reply('#sorry')
+                    }
+                  })
+
                   }else{
                     bp.db.get().then(knex => knex('contacts').whereRaw('LOWER(name) LIKE ?','%'+splitName[1].toLowerCase()+'%').select('name'))
                     .then(suggestions =>{
@@ -141,6 +226,7 @@ module.exports = bp => {
                       }
                     })
                   }
+                });
                 })
               }
             })
@@ -224,34 +310,5 @@ module.exports = bp => {
 
   })
 
-  //trigger for context awareness
-  bp.hear({'nlp.metadata.intentName': 'contact_information_with_contexts'}, (event, next) => {
-
-    const information_type = _.get(event, 'nlp.parameters.information_type')
-    let person = _.get(event, 'nlp.parameters.person')
-
-    bp.db.get().then(knex => knex('contacts').where({name : person}))
-    .then(contact =>{
-      const email = _.get(contact[0],'email')
-      const phoneNumber = _.get(contact[0],'telephone')
-
-      if (information_type == 'Email address') {
-        event.reply('#contactReplyDistinct',{
-          person:person,
-          information: email,
-          information_type:information_type
-        });
-
-      } else if (information_type == 'Phone number') {
-        event.reply('#contactReplyDistinct',{
-          person:person,
-          information: phoneNumber,
-          information_type:information_type
-        });
-
-      }
-    })
-
-})
 
 }
